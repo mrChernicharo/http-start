9fb8ca78a02bf513ca322f7241f34631cc421c0b
mrChernicharo <felipe.chernicharo@gmail.com>
2020-07-29 17:25:24 -0300

    typing requests, Post interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
+import { Post } from "./post.model"
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,9 +19,10 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
-    this.http.get('https://ng-course-backend-85a2d.firebaseio.com/posts.json')
+    this.http
+      .get<{ [key: string]: Post }>('https://ng-course-backend-85a2d.firebaseio.com/posts.json')
       .pipe(map(response => {
-        const postsArray = [];
+        const postsArray: Post[] = [];
         for (const key in response) {
           if (response.hasOwnProperty(key)) {
             postsArray.push({...response[key], id: key})
@@ -31,9 +34,10 @@ export class AppComponent implements OnInit {
     .subscribe(response => console.log(response))
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     console.log(postData)
-    this.http.post(
+    this.http
+      .post<{ name: string }>(
       'https://ng-course-backend-85a2d.firebaseio.com/posts.json',
       postData)
       .subscribe(response => console.log(response))
