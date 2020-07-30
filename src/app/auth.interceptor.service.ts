d9159ffafaa075c62bc4eda8c7ce8f6dc45ddfdc
mrChernicharo <felipe.chernicharo@gmail.com>
2020-07-29 23:01:31 -0300

    Manipular request e response com Interceptors

--- src/app/auth.interceptor.service.ts
+++ src/app/auth.interceptor.service.ts
@@ -1,9 +1,20 @@
-import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEventType } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler) {
 
-    setTimeout(() => console.log('Request on it\'s way!'), 2000)
-    return next.handle(request);
+    const modifiedRequest = request.clone({
+      headers: request.headers.append('Auth', 'xyz123')
+    })
+
+    return next.handle(modifiedRequest).pipe(tap(event => {
+      console.log(event)
+      if (event.type === HttpEventType.Response) {
+        console.log(`Response arrived. Response body`)
+        console.log(event.body)
+      }
+    }))
   }
 }
